ddeeafa5c3814c9ed582e7166d5f2c23cde6a794
cristinagiacobbe <crigiak@gmail.com>
2024-05-04 19:36:22 +0200

    Add partials: footer

---
+++ resources/views/partials/footer.blade.php
@@ -0,0 +1,170 @@
+<footer class="text-center text-lg-start text-white" style="background-color: #3e4551">
+    <!-- Grid container -->
+    <div class="container p-4 pb-0">
+        <!-- Section: Links -->
+        <section>
+
+            <div class="row">
+                <div class="col-lg-5 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">comics</h5>
+                    <p>
+                        Lorem ipsum dolor sit amet consectetur adipisicing elit.
+                        Molestiae modi cum ipsam ad, illo possimus laborum ut
+                        reiciendis obcaecati. Ducimus, quas. Corrupti, pariatur eaque?
+                        Reiciendis assumenda iusto sapiente inventore animi?
+                    </p>
+                </div>
+
+
+                <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">novità</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="#!" class="text-white">Eventi</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Notizie</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Progetti futuri</a>
+                        </li>
+                    </ul>
+                </div>
+
+                <div class="col-lg-3 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">useful links</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="#!" class="text-white">Your account</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Become an affiliate</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Shipping rates</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">Help</a>
+                        </li>
+                    </ul>
+                </div>
+
+
+                <div class="col-lg-2 col-md-6 mb-4 mb-md-0">
+                    <h5 class="text-uppercase">contatti</h5>
+
+                    <ul class="list-unstyled mb-0">
+                        <li>
+                            <a href="#!" class="text-white">Corso Francia 1, 10098 Rivoli (TO)</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">011 9503753</a>
+                        </li>
+                        <li>
+                            <a href="#!" class="text-white">comics@gmail.com</a>
+                        </li>
+                    </ul>
+                </div>
+
+            </div>
+
+        </section>
+
+
+        <hr class="mb-4" />
+
+        {{--  <!-- Section: CTA -->
+            <section class="">
+              <p class="d-flex justify-content-center align-items-center">
+                <span class="me-3">Register for free</span>
+                <button type="button" class="btn btn-outline-light btn-rounded">
+                  Sign up!
+                </button>
+              </p>
+            </section>
+            <!-- Section: CTA -->
+      
+            <hr class="mb-4" />
+      
+            <!-- Section: Social media -->
+            <section class="mb-4 text-center">
+              <!-- Facebook -->
+              <a
+                 class="btn btn-outline-light btn-floating m-1"
+                 href="#!"
+                 role="button"
+                 ><i class="fab fa-facebook-f"></i
+                ></a>
+      
+              <!-- Twitter -->
+              <a
+                 class="btn btn-outline-light btn-floating m-1"
+                 href="#!"
+                 role="button"
+                 ><i class="fab fa-twitter"></i
+                ></a>
+      
+              <!-- Google -->
+              <a
+                 class="btn btn-outline-light btn-floating m-1"
+                 href="#!"
+                 role="button"
+                 ><i class="fab fa-google"></i
+                ></a>
+      
+              <!-- Instagram -->
+              <a
+                 class="btn btn-outline-light btn-floating m-1"
+                 href="#!"
+                 role="button"
+                 ><i class="fab fa-instagram"></i
+                ></a>
+      
+              <!-- Linkedin -->
+              <a
+                 class="btn btn-outline-light btn-floating m-1"
+                 href="#!"
+                 role="button"
+                 ><i class="fab fa-linkedin-in"></i
+                ></a>
+      
+              <!-- Github -->
+              <a
+                 class="btn btn-outline-light btn-floating m-1"
+                 href="#!"
+                 role="button"
+                 ><i class="fab fa-github"></i
+                ></a>
+            </section>
+            <!-- Section: Social media -->
+          </div>
+          <!-- Grid container -->
+      
+          <!-- Copyright -->
+          <div
+               class="text-center p-3"
+               style="background-color: rgba(0, 0, 0, 0.2)"
+               >
+            © 2020 Copyright:
+            <a class="text-white" href="https://mdbootstrap.com/"
+               >MDBootstrap.com</a
+              >
+          </div> --}}
+        <!-- Copyright -->
+</footer>
+<!-- Footer -->
+
+<!-- End of .container -->
+
+
+
+
+
+
+
+
+
+
+</footer>
